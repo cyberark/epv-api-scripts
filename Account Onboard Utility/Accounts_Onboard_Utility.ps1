015aa292ae8673bc9b1607fb27431e0dcace039b
Assaf Miron <AssafMiron@users.noreply.github.com>
2021-01-24 13:07:57 +0200

    updating version number

--- Account Onboard Utility/Accounts_Onboard_Utility.ps1
+++ Account Onboard Utility/Accounts_Onboard_Utility.ps1
@@ -72,7 +72,7 @@ $PSBoundParameters.GetEnumerator() | ForEach-Object { $ScriptParameters += ("-{0
 $global:g_ScriptCommand = "{0} {1}" -f $ScriptFullPath, $($ScriptParameters -join ' ')
 
 # Script Version
-$ScriptVersion = "2.4"
+$ScriptVersion = "2.5"
 
 # Set Log file path
 $LOG_FILE_PATH = "$ScriptLocation\Account_Onboarding_Utility.log"
