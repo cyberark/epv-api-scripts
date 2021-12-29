a13f0d83aa7bd26b57f59074ae2f8d4e78676a3f
bab29 <68295571+bab29@users.noreply.github.com>
2021-12-29 10:05:50 -0800

    Fixeds

--- Platforms/Export-Import-Platform.ps1
+++ Platforms/Export-Import-Platform.ps1
@@ -12,10 +12,10 @@ CyberArk PVWA v10.4 and above
 
 VERSION HISTORY:
 1.0 05/07/2018 - Initial release
-1.1 08/12/2021 - Added ability to do bulk export/import
+1.1 08/12/2018 - Added ability to do bulk export/import
 
 ########################################################################### #>
-[CmdletBinding()]
+
 param
 (
 	[Parameter(Mandatory = $true, HelpMessage = "Please enter your PVWA address (For example: https://pvwa.mydomain.com/PasswordVault)")]
@@ -26,10 +26,6 @@ param
 	[Parameter(Mandatory = $false, HelpMessage = "Enter the Authentication type (Default:CyberArk)")]
 	[ValidateSet("cyberark", "ldap", "radius")]
 	[String]$AuthType = "cyberark",	
-  
-  [Parameter(Mandatory=$false,HelpMessage="Enter the RADIUS OTP")]
-	[ValidateScript({$AuthType -eq "radius"})]
-	[String]$OTP,
 	
 	# Use this switch to Import a Platform
 	[Parameter(ParameterSetName = 'Import', Mandatory = $true)][switch]$Import,
@@ -69,24 +65,6 @@ param
 	[Switch]$DisableSSLVerify
 )
 
-# Get Script Location 
-$ScriptFullPath = $MyInvocation.MyCommand.Path
-$ScriptLocation = Split-Path -Parent $ScriptFullPath
-$ScriptParameters = @()
-$PSBoundParameters.GetEnumerator() | ForEach-Object { $ScriptParameters += ("-{0} '{1}'" -f $_.Key, $_.Value) }
-$global:g_ScriptCommand = "{0} {1}" -f $ScriptFullPath, $($ScriptParameters -join ' ')
-
-# Script Version
-$ScriptVersion = "2.0"
-
-# Set Log file path
-$LOG_FILE_PATH = "$ScriptLocation\Export-Import-Platforms.log"
-
-$InDebug = $PSBoundParameters.Debug.IsPresent
-$InVerbose = $PSBoundParameters.Verbose.IsPresent
-# Set a global Header Token parameter
-$global:g_LogonHeader = ""
-
 # Global URLS
 # -----------
 $URL_PVWAAPI = $PVWAURL + "/api"
@@ -96,10 +74,15 @@ $URL_Logoff = $URL_Authentication + "/Logoff"
 
 # URL Methods
 # -----------
-$URL_Platforms = $URL_PVWAAPI+"/Platforms"
-$URL_PlatformDetails = $URL_Platforms+"/{0}"
-$URL_ExportPlatforms = $URL_PlatformDetails+"/Export"
-$URL_ImportPlatforms = $URL_Platforms+"/Import"
+$URL_GetPlatforms = $URL_PVWAAPI + "/Platforms"
+$URL_PlatformDetails = $URL_PVWAAPI + "/Platforms/{0}"
+$URL_ExportPlatforms = $URL_PVWAAPI + "/Platforms/{0}/Export"
+$URL_ImportPlatforms = $URL_PVWAAPI + "/Platforms/Import"
+
+# Initialize Script Variables
+# ---------------------------
+$rstusername = $rstpassword = ""
+$logonToken = ""
 
 #region Functions
 Function Test-CommandExists {
@@ -110,111 +93,6 @@ Function Test-CommandExists {
 	Catch { Write-Host "$command does not exist"; RETURN $false }
 	Finally { $ErrorActionPreference = $oldPreference }
 } #end function test-CommandExists
-
-#region Log Functions
-# @FUNCTION@ ======================================================================================================================
-# Name...........: Write-LogMessage
-# Description....: Writes the message to log and screen
-# Parameters.....: MSG, (Switch)Header, (Switch)SubHeader, (Switch)Footer, Type
-# Return Values..: None
-# =================================================================================================================================
-Function Write-LogMessage
-{
-<# 
-.SYNOPSIS 
-	Method to log a message on screen and in a log file
-.DESCRIPTION
-	Logging The input Message to the Screen and the Log File. 
-	The Message Type is presented in colours on the screen based on the type
-.PARAMETER MSG
-	The message to log
-.PARAMETER Header
-	Adding a header line before the message
-.PARAMETER SubHeader
-	Adding a Sub header line before the message
-.PARAMETER Footer
-	Adding a footer line after the message
-.PARAMETER Type
-	The type of the message to log (Info, Warning, Error, Debug)
-#>
-	param(
-		[Parameter(Mandatory=$true)]
-		[AllowEmptyString()]
-		[String]$MSG,
-		[Parameter(Mandatory=$false)]
-		[Switch]$Header,
-		[Parameter(Mandatory=$false)]
-		[Switch]$SubHeader,
-		[Parameter(Mandatory=$false)]
-		[Switch]$Footer,
-		[Parameter(Mandatory=$false)]
-		[ValidateSet("Info","Warning","Error","Debug","Verbose")]
-		[String]$type = "Info"
-	)
-	try{
-		If ($Header) {
-			"=======================================" | Out-File -Append -FilePath $LOG_FILE_PATH 
-			Write-Host "======================================="
-		}
-		ElseIf($SubHeader) { 
-			"------------------------------------" | Out-File -Append -FilePath $LOG_FILE_PATH 
-			Write-Host "------------------------------------"
-		}
-	
-		$msgToWrite = "[$(Get-Date -Format "yyyy-MM-dd hh:mm:ss")]`t"
-		$writeToFile = $true
-		# Replace empty message with 'N/A'
-		if([string]::IsNullOrEmpty($Msg)) { $Msg = "N/A" }
-		# Mask Passwords
-		if($Msg -match '((?:"password"|"secret"|"NewCredentials")\s{0,}["\:=]{1,}\s{0,}["]{0,})(?=([\w!@#$%^&*()\[\]\-\\\/]+))')
-		{
-			$Msg = $Msg.Replace($Matches[2],"****")
-		}
-		# Check the message type
-		switch ($type)
-		{
-			"Info" { 
-				Write-Host $MSG.ToString()
-				$msgToWrite += "[INFO]`t$Msg"
-				break
-			}
-			"Warning" {
-				Write-Host $MSG.ToString() -ForegroundColor Yellow
-				$msgToWrite += "[WARNING]`t$Msg"
-				break
-			}
-			"Error" {
-				Write-Host $MSG.ToString() -ForegroundColor Red
-				$msgToWrite += "[ERROR]`t$Msg"
-				break
-			}
-			"Debug" { 
-				if($InDebug)
-				{
-					Write-LogMessage -Type Debug -Msg $MSG
-					$msgToWrite += "[DEBUG]`t$Msg"
-				}
-				else { $writeToFile = $False }
-				break
-			}
-			"Verbose" { 
-				if($InVerbose)
-				{
-					Write-Verbose $MSG
-					$msgToWrite += "[VERBOSE]`t$Msg"
-				}
-				else { $writeToFile = $False }
-				break
-			}
-		}
-		
-		If($writeToFile) { $msgToWrite | Out-File -Append -FilePath $LOG_FILE_PATH }
-		If ($Footer) { 
-			"=======================================" | Out-File -Append -FilePath $LOG_FILE_PATH 
-			Write-Host "======================================="
-		}
-	} catch { Write-Error "Error in writing log: $($_.Exception.Message)" }
-}
 
 # @FUNCTION@ ======================================================================================================================
 # Name...........: Join-ExceptionMessage
@@ -248,169 +126,7 @@ Function Join-ExceptionMessage {
 	End {
 	}
 }
-#endregion
 
-Function Get-LogonHeader
-{
-    <# 
-.SYNOPSIS 
-	Get-LogonHeader
-.DESCRIPTION
-	Get-LogonHeader
-.PARAMETER Credentials
-	The REST API Credentials to authenticate
-#>
-    param(
-        [Parameter(Mandatory = $true)]
-        [PSCredential]$Credentials,
-        [Parameter(Mandatory = $false)]
-		    [string]$RadiusOTP
-    )
-	
-    if ([string]::IsNullOrEmpty($g_LogonHeader))
-    {
-        # Disable SSL Verification to contact PVWA
-        If ($DisableSSLVerify)
-        {
-            Disable-SSLVerification
-        }
-		
-        # Create the POST Body for the Logon
-        # ----------------------------------
-        If ($ConnectionNumber -eq 0)
-        {
-            $logonBody = @{ username = $Credentials.username.Replace('\', ''); password = $Credentials.GetNetworkCredential().password } | ConvertTo-Json
-        }
-        elseif ($ConnectionNumber -gt 0)
-        {
-            $logonBody = @{ username = $Credentials.username.Replace('\', ''); password = $Credentials.GetNetworkCredential().password; connectionNumber = $ConnectionNumber } | ConvertTo-Json
-        }
-        try
-        {
-            # Logon
-            $logonToken = Invoke-RestMethod -Method Post -Uri $URL_Logon -Body $logonBody -ContentType "application/json" -TimeoutSec 2700
-			
-            # Clear logon body
-            $logonBody = ""
-        }
-        catch
-        {
-            Throw $(New-Object System.Exception ("Get-LogonHeader: $($_.Exception.Response.StatusDescription)", $_.Exception))
-        }
-
-        $logonHeader = $null
-        If ([string]::IsNullOrEmpty($logonToken))
-        {
-            Throw "Get-LogonHeader: Logon Token is Empty - Cannot login"
-        }
-		
-        try
-        {
-            # Create a Logon Token Header (This will be used through out all the script)
-            # ---------------------------
-            If ($logonToken.PSObject.Properties.Name -contains "CyberArkLogonResult")
-            {
-                $logonHeader = @{Authorization = $($logonToken.CyberArkLogonResult) }
-            }
-            else
-            {
-                $logonHeader = @{Authorization = $logonToken }
-            }	
-
-            Set-Variable -Name g_LogonHeader -Value $logonHeader -Scope global		
-        }
-        catch
-        {
-            Throw $(New-Object System.Exception ("Get-LogonHeader: Could not create Logon Headers Dictionary", $_.Exception))
-        }
-    }
-}
-
-Function Invoke-Logoff
-{
-    <# 
-.SYNOPSIS 
-	Invoke-Logoff
-.DESCRIPTION
-	Logoff a PVWA session
-#>
-    try
-    {
-        # Logoff the session
-        # ------------------
-        If ($null -ne $g_LogonHeader)
-        {
-            Write-LogMessage -Type Info -Msg "Logoff Session..."
-            Invoke-RestMethod -Method Post -Uri $URL_Logoff -Headers $g_LogonHeader -ContentType "application/json" -TimeoutSec 2700 | Out-Null
-            Set-Variable -Name g_LogonHeader -Value $null -Scope global
-        }
-    }
-    catch
-    {
-        Throw $(New-Object System.Exception ("Invoke-Logoff: Failed to logoff session", $_.Exception))
-    }
-}
-
-Function Disable-SSLVerification
-{
-    <# 
-.SYNOPSIS 
-	Bypass SSL certificate validations
-.DESCRIPTION
-	Disables the SSL Verification (bypass self signed SSL certificates)
-#>
-    # Check if to disable SSL verification
-    If ($DisableSSLVerify)
-    {
-        try
-        {
-            Write-Warning "It is not Recommended to disable SSL verification" -WarningAction Inquire
-            # Using Proxy Default credentials if the Server needs Proxy credentials
-            [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
-            # Using TLS 1.2 as security protocol verification
-            [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-            # Disable SSL Verification
-            if (-not("DisableCertValidationCallback" -as [type]))
-            {
-                Add-Type -TypeDefinition @"
-using System;
-using System.Net;
-using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
-
-public static class DisableCertValidationCallback {
-    public static bool ReturnTrue(object sender,
-        X509Certificate certificate,
-        X509Chain chain,
-        SslPolicyErrors sslPolicyErrors) { return true; }
-
-    public static RemoteCertificateValidationCallback GetDelegate() {
-        return new RemoteCertificateValidationCallback(DisableCertValidationCallback.ReturnTrue);
-    }
-}
-"@ 
-            }
-
-            [System.Net.ServicePointManager]::ServerCertificateValidationCallback = [DisableCertValidationCallback]::GetDelegate()
-        }
-        catch
-        {
-            Write-LogMessage -Type Error -Msg "Could not change SSL validation. Error: $(Join-ExceptionMessage $_.Exception)"
-        }
-    }
-    Else
-    {
-        try
-        {
-            Write-LogMessage -Type Info -Msg "Setting script to use TLS 1.2"
-            [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-        }
-        catch
-        {
-            Write-LogMessage -Type Error -Msg "Could not change SSL setting to use TLS 1.2. Error: $(Join-ExceptionMessage $_.Exception)"
-        }
-    }
-}
 #endregion
 
 Function import-platform {
@@ -419,15 +135,14 @@ Function import-platform {
 		[string]$PlatformZipPath
 	)
 	If (Test-Path $PlatformZipPath) {
-		# Converting to Base64, following bug 00015428
-		$zipContent = [Convert]::ToBase64String([System.IO.File]::ReadAllBytes($(Resolve-Path $PlatformZipPath)))
+		$zipContent = [System.IO.File]::ReadAllBytes($(Resolve-Path $PlatformZipPath))
 		$importBody = @{ ImportFile = $zipContent; } | ConvertTo-Json -Depth 3 -Compress
 		try {
-			$ImportPlatformResponse = Invoke-RestMethod -Method POST -Uri $URL_ImportPlatforms -Headers $g_LogonHeader -ContentType "application/json" -TimeoutSec 2700 -Body $importBody
+			$ImportPlatformResponse = Invoke-RestMethod -Method POST -Uri $URL_ImportPlatforms -Headers $logonHeader -ContentType "application/json" -TimeoutSec 2700 -Body $importBody
 			Write-Debug "Platform ID imported: $($ImportPlatformResponse.PlatformID)"
 			Write-Host "Retrieving Platform details"
 			# Get the Platform Name
-			$platformDetails = Invoke-RestMethod -Method Get -Uri $($URL_PlatformDetails -f $ImportPlatformResponse.PlatformID) -Headers $g_LogonHeader -ContentType "application/json" -TimeoutSec 2700
+			$platformDetails = Invoke-RestMethod -Method Get -Uri $($URL_PlatformDetails -f $ImportPlatformResponse.PlatformID) -Headers $logonHeader -ContentType "application/json" -TimeoutSec 2700
 			If ($platformDetails) {
 				Write-Debug $platformDetails
 				Write-Host "$($platformDetails.Details.PolicyName) (ID: $($platformDetails.PlatformID)) was successfully imported and $(if($platformDetails.Active) { "Activated" } else { "Inactive" })"
@@ -454,7 +169,7 @@ Function export-platform {
 
 	try {
 		$exportURL = $URL_ExportPlatforms -f $PlatformID
-		Invoke-RestMethod -Method POST -Uri $exportURL -Headers $g_LogonHeader -ContentType "application/zip" -TimeoutSec 2700 -OutFile "$PlatformZipPath\$PlatformID.zip" -ErrorAction SilentlyContinue
+		Invoke-RestMethod -Method POST -Uri $exportURL -Headers $logonHeader -ContentType "application/zip" -TimeoutSec 2700 -OutFile "$PlatformZipPath\$PlatformID.zip" -ErrorAction SilentlyContinue
 	} catch {
 		#Write-Error $_.Exception.Response
 		#Write-Error $_.Exception.Response.StatusDescription
@@ -479,7 +194,7 @@ Function Get-PlatformsList {
 			$url = $URL_GetPlatforms + "?Active=True&PlatformType=Regular"
 		}
 		
-		$result = Invoke-RestMethod -Method GET -Uri $url -Headers $g_LogonHeader -ErrorAction SilentlyContinue
+		$result = Invoke-RestMethod -Method GET -Uri $url -Headers $logonHeader -ErrorAction SilentlyContinue
 
 		foreach ($platform in $result.Platforms){
 			$idList += $platform.general.id
@@ -543,8 +258,31 @@ If (Test-CommandExists Invoke-RestMethod) {
 	$msg = "Enter your User name and Password"; 
 	if ($Null -eq $creds) {$creds = $Host.UI.PromptForCredential($caption, $msg, "", "")}
 	if ($null -ne $creds) {
-		Get-LogonHeader $creds -RadiusOTP $OTP
+		$rstusername = $creds.username.Replace('\', '');    
+		$rstpassword = $creds.GetNetworkCredential().password
+
 	} else { return }
+
+	# Create the POST Body for the Logon
+	# ----------------------------------
+	$logonBody = @{ username = $rstusername; password = $rstpassword; concurrentSession = 'true' }
+	$logonBody = $logonBody | ConvertTo-Json
+	try {
+		# Logon
+		$logonToken = Invoke-RestMethod -Method Post -Uri $URL_Logon -Body $logonBody -ContentType "application/json"
+	} catch {
+		Write-Host -ForegroundColor Red $_.Exception.Response.StatusDescription
+		$logonToken = ""
+	}
+	If ($logonToken -eq "") {
+		Write-Host -ForegroundColor Red "Logon Token is Empty - Cannot login"
+		return
+	}
+	
+	# Create a Logon Token Header (This will be used through out all the script)
+	# ---------------------------
+	$logonHeader = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+	$logonHeader.Add("Authorization", $logonToken)
 	#endregion
 
 	switch ($PsCmdlet.ParameterSetName) {
@@ -583,7 +321,10 @@ If (Test-CommandExists Invoke-RestMethod) {
 		}
 	}
 	# Logoff the session
-	Invoke-Logoff
+
+	# ------------------
+	Write-Host "Logoff Session..."
+	Invoke-RestMethod -Method Post -Uri $URL_Logoff -Headers $logonHeader -ContentType "application/json" | Out-Null
 } else {
 	Write-Error "This script requires PowerShell version 3 or above"
 }
