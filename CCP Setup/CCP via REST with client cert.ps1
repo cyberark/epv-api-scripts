6df1266d13984b1de87afecb98863ed7d1717119
bab29 <68295571+bab29@users.noreply.github.com>
2023-01-04 09:53:38 -0800

    Updated to allow for Object Name OR Address and username

--- CCP Setup/CCP via REST with client cert.ps1
+++ CCP Setup/CCP via REST with client cert.ps1
@@ -1,19 +1,34 @@
-$response = $null
-$CCPAddress="http://ccp.lab.local"
+$responseViaObject = $responseViaAddressAndUsername =  $null
+$CCPAddress = "http://ccp.lab.local"
 $application = "app"
 $safe = "Safe"
-$object ="object"
+$object = ""
+$address = ""
+$username = ""
+
 $certThumbprint = ""
-
 $cert = Get-ChildItem Cert:\LocalMachine\My\$certThumbprint
-
-$response = $null
 
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Content-Type", "application/json")
 
-$response = Invoke-RestMethod "$CCPAddress/AIMWebService/api/Accounts?AppID=$application&Safe=$safe&Object=$object" -Method 'GET' -Headers $headers -Certificate $cert 
-$response | ConvertTo-Json
+if (![string]::IsNullOrEmpty($object)) {
+    $responseViaObject = Invoke-RestMethod "$CCPAddress/AIMWebService/api/Accounts?AppID=$application&Safe=$safe&Object=$object" -Method 'GET' -Headers $headers  -Certificate $cert 
 
-write-host "Username: " $($response.Username)
-Write-Host "Password: " $($response.content)
+    Write-Host "Pulled using Object Name"
+    write-host "Username: " $($responseViaObject.Username)
+    Write-Host "Password: " $($responseViaObject.content)
+    ""
+    $responseViaObject | ConvertTo-Json
+}
+
+if (![string]::IsNullOrEmpty($address) -and ![string]::IsNullOrEmpty($username)) {
+    $responseViaAddressAndUsername = Invoke-RestMethod "$CCPAddress/AIMWebService/api/Accounts?AppID=$application&Safe=$safe&address=$address&username=$username" -Method 'GET' -Headers $headers  -Certificate $cert 
+    $responseViaAddressAndUsername | ConvertTo-Json
+
+    Write-Host "Pulled using Address and Username"
+    write-host "Username: " $($responseViaAddressAndUsername.Username)
+    Write-Host "Password: " $($responseViaAddressAndUsername.content)
+    ""
+    $responseViaAddressAndUsername | ConvertTo-Json
+}
