06503ce7212ca1af2de09558e82b333553658106
bab29 <68295571+bab29@users.noreply.github.com>
2023-06-25 11:25:21 -0700

    Version update

--- Account Onboard Utility/Accounts_Onboard_Utility.ps1
+++ Account Onboard Utility/Accounts_Onboard_Utility.ps1
@@ -127,7 +127,7 @@ $PSBoundParameters.GetEnumerator() | ForEach-Object { $ScriptParameters += ("-{0
 $global:g_ScriptCommand = "{0} {1}" -f $ScriptFullPath, $($ScriptParameters -join ' ')
 
 # Script Version
-$ScriptVersion = "2.4.1"
+$ScriptVersion = "2.4.2"
 
 # Set Log file path
 $global:LOG_DATE = $(Get-Date -Format yyyyMMdd) + "-" + $(Get-Date -Format HHmmss)
