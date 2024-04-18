4fc67c031bed4afddd6ce134cfb6fd81c96c311d
bab29 <68295571+bab29@users.noreply.github.com>
2024-04-18 12:02:35 -0700

    Update Safe-Management.ps1

--- Safe Management/Safe-Management.ps1
+++ Safe Management/Safe-Management.ps1
@@ -27,6 +27,7 @@
  2.1.6  2023-05-22      - Updated Write-LogMessage to force verbose and debug to log file
  2.1.7  2024-04-17      - Updated to bypass attempt to add or update safe if no safe details exist
  2.1.8  2024-04-18      - Added ability to force Safe Creations
+                   	- Added "AddMember" back
 ########################################################################### #>
 [CmdletBinding(DefaultParameterSetName = 'List')]
 param
@@ -49,6 +50,8 @@ param
     [Parameter(ParameterSetName = 'Add', Mandatory = $true)][switch]$Add,
     # Use this switch to Update Safes
     [Parameter(ParameterSetName = 'Update', Mandatory = $true)][switch]$Update,
+    # Use this switch to Update Safe Members
+    [Parameter(ParameterSetName = 'AddMembers', Mandatory = $true)][switch]$AddMembers,
     # Use this switch to Update Safe Members
     [Parameter(ParameterSetName = 'UpdateMembers', Mandatory = $true)][switch]$UpdateMembers,
     # Use this switch to Delete Safe Members
@@ -77,6 +80,7 @@ param
     # Import File support
     [Parameter(ParameterSetName = 'Add', Mandatory = $false, HelpMessage = 'Enter a file path for bulk safe creation')]
     [Parameter(ParameterSetName = 'Update', Mandatory = $false, HelpMessage = 'Enter a file path for bulk safe update')]
+    [Parameter(ParameterSetName = 'AddMembers', Mandatory = $false, HelpMessage = 'Enter a file path for bulk safe membership update')]
     [Parameter(ParameterSetName = 'UpdateMembers', Mandatory = $false, HelpMessage = 'Enter a file path for bulk safe membership update')]
     [Parameter(ParameterSetName = 'DeleteMembers', Mandatory = $false, HelpMessage = 'Enter a file path for bulk safe membership deletion')]
     [Parameter(ParameterSetName = 'Delete', Mandatory = $false, HelpMessage = 'Enter a file path for bulk safe deletion')]
@@ -138,7 +142,7 @@ param
     $logonToken,
     # Use this switch to create safes when only safe name provided
     [Parameter(Mandatory = $false)]
-    [Switch]$eeWithNameOnly
+    [Switch]$CreateSafeWithNameOnly
 )
 
 # Get Script Location 
