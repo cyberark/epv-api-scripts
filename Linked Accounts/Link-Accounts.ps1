af4531a4d99264243aa129234d7abfe2914c7de2
Brian A. Bors <brian.bors@cyberark.com>
2025-04-09 14:12:31 -0700

    Fix to Get-CallStack

--- Linked Accounts/Link-Accounts.ps1
+++ Linked Accounts/Link-Accounts.ps1
@@ -39,11 +39,11 @@ param
 	[Parameter(Mandatory = $false)]
 	$logonToken,
 
-    [Parameter(Mandatory = $false, DontShow, HelpMessage = 'Include Call Stack in Verbose output')]
-    [switch]$IncludeCallStack,
+	[Parameter(Mandatory = $false, DontShow, HelpMessage = 'Include Call Stack in Verbose output')]
+	[switch]$IncludeCallStack,
 
-    [Parameter(Mandatory = $false, DontShow)]
-    [switch]$UseVerboseFile
+	[Parameter(Mandatory = $false, DontShow)]
+	[switch]$UseVerboseFile
 
 )
 
@@ -201,7 +201,7 @@ Function ConvertTo-EPOCHDate($inputDate) {
 
 
 Function Write-LogMessage {
-    <#
+	<#
 .SYNOPSIS
 	Method to log a message on screen and in a log file
 
@@ -222,139 +222,139 @@ Function Write-LogMessage {
 .PARAMETER Type
 	The type of the message to log (Info, Warning, Error, Debug)
 #>
-    param(
-        [Parameter(Mandatory = $true)]
-        [AllowEmptyString()]
-        [String]$MSG,
-        [Parameter(Mandatory = $false)]
-        [Switch]$Header,
-        [Parameter(Mandatory = $false)]
-        [Switch]$SubHeader,
-        [Parameter(Mandatory = $false)]
-        [Switch]$Footer,
-        [Parameter(Mandatory = $false)]
-        [ValidateSet('Info', 'Warning', 'Error', 'Debug', 'Verbose')]
-        [String]$type = 'Info',
-        [Parameter(Mandatory = $false)]
-        [String]$LogFile = $LOG_FILE_PATH,
-        [Parameter(Mandatory = $false)]
-        [int]$pad = 20
-    )
+	param(
+		[Parameter(Mandatory = $true)]
+		[AllowEmptyString()]
+		[String]$MSG,
+		[Parameter(Mandatory = $false)]
+		[Switch]$Header,
+		[Parameter(Mandatory = $false)]
+		[Switch]$SubHeader,
+		[Parameter(Mandatory = $false)]
+		[Switch]$Footer,
+		[Parameter(Mandatory = $false)]
+		[ValidateSet('Info', 'Warning', 'Error', 'Debug', 'Verbose')]
+		[String]$type = 'Info',
+		[Parameter(Mandatory = $false)]
+		[String]$LogFile = $LOG_FILE_PATH,
+		[Parameter(Mandatory = $false)]
+		[int]$pad = 20
+	)
 
-    $verboseFile = $($LOG_FILE_PATH.replace('.log', '_Verbose.log'))
-    try {
-        If ($Header) {
-            '=======================================' | Out-File -Append -FilePath $LOG_FILE_PATH
-            Write-Host '======================================='
-        }
-        ElseIf ($SubHeader) {
-            '------------------------------------' | Out-File -Append -FilePath $LOG_FILE_PATH
-            Write-Host '------------------------------------'
-        }
+	$verboseFile = $($LOG_FILE_PATH.replace('.log', '_Verbose.log'))
+	try {
+		If ($Header) {
+			'=======================================' | Out-File -Append -FilePath $LOG_FILE_PATH
+			Write-Host '======================================='
+		}
+		ElseIf ($SubHeader) {
+			'------------------------------------' | Out-File -Append -FilePath $LOG_FILE_PATH
+			Write-Host '------------------------------------'
+		}
 
-        $LogTime = "[$(Get-Date -Format 'yyyy-MM-dd hh:mm:ss')]`t"
-        $msgToWrite += "$LogTime"
-        $writeToFile = $true
-        # Replace empty message with 'N/A'
-        if ([string]::IsNullOrEmpty($Msg)) {
-            $Msg = 'N/A'
-        }
-        # Mask Passwords
-        if ($Msg -match '((?:"password"|"secret"|"NewCredentials")\s{0,}["\:=]{1,}\s{0,}["]{0,})(?=([\w!@#$%^&*()-\\\/]+))') {
-            $Msg = $Msg.Replace($Matches[2], '****')
-        }
-        # Check the message type
-        switch ($type) {
-            'Info' {
-                Write-Host $MSG.ToString()
-                $msgToWrite += "[INFO]`t`t$Msg"
-            }
-            'Warning' {
-                Write-Host $MSG.ToString() -ForegroundColor DarkYellow
-                $msgToWrite += "[WARNING]`t$Msg"
-                if ($UseVerboseFile) {
-                    $msgToWrite | Out-File -Append -FilePath $verboseFile
-                }
-            }
-            'Error' {
-                Write-Host $MSG.ToString() -ForegroundColor Red
-                $msgToWrite += "[ERROR]`t$Msg"
-                if ($UseVerboseFile) {
-                    $msgToWrite | Out-File -Append -FilePath $verboseFile
-                }
-            }
-            'Debug' {
-                if ($InDebug -or $InVerbose) {
-                    Write-Debug $MSG
-                    $writeToFile = $true
-                    $msgToWrite += "[DEBUG]`t$Msg"
-                }
-                else {
-                    $writeToFile = $False
-                }
-            }
-            'Verbose' {
-                if ($InVerbose -or $VerboseFile) {
-                    $arrMsg = $msg.split(":`t", 2)
-                    if ($arrMsg.Count -gt 1) {
-                        $msg = $arrMsg[0].PadRight($pad) + $arrMsg[1]
-                    }
-                    $msgToWrite += "[VERBOSE]`t$Msg"
-                    if ($global:IncludeCallStack) {
-                        function Get-CallStack {
-                            $stack = ''
-                            $excludeItems = @('Write-LogMessage', 'Get-CallStack', '<ScriptBlock>')
-                            Get-PSCallStack | ForEach-Object {
-                                If ($PSItem.Command -notin $excludeItems) {
-                                    $command = $PSitem.Command
-                                    If ($command -eq $Global:scriptName) {
-                                        $command = 'Base'
-                                    }
-                                    elseif ([string]::IsNullOrEmpty($command)) {
-                                        $command = '**Blank**'
-                                    }
-                                    $Location = $PSItem.Location
-                                    $stack = $stack + "$command $Location; "
-                                }
-                            }
-                            return $stack
-                        }
-                        $stack = Get-CallStack
-                        $stackMsg = "CallStack:`t$stack"
-                        $arrstackMsg = $stackMsg.split(":`t", 2)
-                        if ($arrMsg.Count -gt 1) {
-                            $stackMsg = $arrstackMsg[0].PadRight($pad) + $arrstackMsg[1].trim()
-                        }
-                        Write-Verbose $stackMsg
-                        $msgToWrite += "`n$LogTime"
-                        $msgToWrite += "[STACK]`t`t$stackMsg"
-                    }
-                    if ($InVerbose) {
-                        Write-Verbose $MSG
-                    }
-                    else {
-                        $writeToFile = $False
-                    }
-                    if ($UseVerboseFile) {
-                        $msgToWrite | Out-File -Append -FilePath $verboseFile
-                    }
-                }
-                else {
-                    $writeToFile = $False
-                }
-            }
-        }
-        If ($writeToFile) {
-            $msgToWrite | Out-File -Append -FilePath $LOG_FILE_PATH
-        }
-        If ($Footer) {
-            '=======================================' | Out-File -Append -FilePath $LOG_FILE_PATH
-            Write-Host '======================================='
-        }
-    }
-    catch {
-        Write-Error "Error in writing log: $($_.Exception.Message)"
-    }
+		$LogTime = "[$(Get-Date -Format 'yyyy-MM-dd hh:mm:ss')]`t"
+		$msgToWrite += "$LogTime"
+		$writeToFile = $true
+		# Replace empty message with 'N/A'
+		if ([string]::IsNullOrEmpty($Msg)) {
+			$Msg = 'N/A'
+		}
+		# Mask Passwords
+		if ($Msg -match '((?:"password"|"secret"|"NewCredentials")\s{0,}["\:=]{1,}\s{0,}["]{0,})(?=([\w!@#$%^&*()-\\\/]+))') {
+			$Msg = $Msg.Replace($Matches[2], '****')
+		}
+		# Check the message type
+		switch ($type) {
+			'Info' {
+				Write-Host $MSG.ToString()
+				$msgToWrite += "[INFO]`t`t$Msg"
+			}
+			'Warning' {
+				Write-Host $MSG.ToString() -ForegroundColor DarkYellow
+				$msgToWrite += "[WARNING]`t$Msg"
+				if ($UseVerboseFile) {
+					$msgToWrite | Out-File -Append -FilePath $verboseFile
+				}
+			}
+			'Error' {
+				Write-Host $MSG.ToString() -ForegroundColor Red
+				$msgToWrite += "[ERROR]`t$Msg"
+				if ($UseVerboseFile) {
+					$msgToWrite | Out-File -Append -FilePath $verboseFile
+				}
+			}
+			'Debug' {
+				if ($InDebug -or $InVerbose) {
+					Write-Debug $MSG
+					$writeToFile = $true
+					$msgToWrite += "[DEBUG]`t$Msg"
+				}
+				else {
+					$writeToFile = $False
+				}
+			}
+			'Verbose' {
+				if ($InVerbose -or $VerboseFile) {
+					$arrMsg = $msg.split(":`t", 2)
+					if ($arrMsg.Count -gt 1) {
+						$msg = $arrMsg[0].PadRight($pad) + $arrMsg[1]
+					}
+					$msgToWrite += "[VERBOSE]`t$Msg"
+					if ($global:IncludeCallStack) {
+						function Get-CallStack {
+							$stack = ''
+							$excludeItems = @('Write-LogMessage', 'Get-CallStack', '<ScriptBlock>')
+							Get-CallStack | ForEach-Object {
+								If ($PSItem.Command -notin $excludeItems) {
+									$command = $PSitem.Command
+									If ($command -eq $Global:scriptName) {
+										$command = 'Base'
+									}
+									elseif ([string]::IsNullOrEmpty($command)) {
+										$command = '**Blank**'
+									}
+									$Location = $PSItem.Location
+									$stack = $stack + "$command $Location; "
+								}
+							}
+							return $stack
+						}
+						$stack = Get-CallStack
+						$stackMsg = "CallStack:`t$stack"
+						$arrstackMsg = $stackMsg.split(":`t", 2)
+						if ($arrMsg.Count -gt 1) {
+							$stackMsg = $arrstackMsg[0].PadRight($pad) + $arrstackMsg[1].trim()
+						}
+						Write-Verbose $stackMsg
+						$msgToWrite += "`n$LogTime"
+						$msgToWrite += "[STACK]`t`t$stackMsg"
+					}
+					if ($InVerbose) {
+						Write-Verbose $MSG
+					}
+					else {
+						$writeToFile = $False
+					}
+					if ($UseVerboseFile) {
+						$msgToWrite | Out-File -Append -FilePath $verboseFile
+					}
+				}
+				else {
+					$writeToFile = $False
+				}
+			}
+		}
+		If ($writeToFile) {
+			$msgToWrite | Out-File -Append -FilePath $LOG_FILE_PATH
+		}
+		If ($Footer) {
+			'=======================================' | Out-File -Append -FilePath $LOG_FILE_PATH
+			Write-Host '======================================='
+		}
+	}
+	catch {
+		Write-Error "Error in writing log: $($_.Exception.Message)"
+	}
 }
 
 
@@ -424,7 +424,7 @@ Function Open-FileDialog($initialDirectory) {
 }
 
 Function Invoke-Rest {
-    <#
+	<#
 .SYNOPSIS
 Invoke REST Method
 .DESCRIPTION
@@ -440,90 +440,90 @@ The Header as Dictionary object
 .PARAMETER ErrAction
 (Optional) The Error Action to perform in case of error. By default "Continue"
 #>
-    param (
-        [Parameter(Mandatory = $true)]
-        [ValidateSet('GET', 'POST', 'DELETE', 'PATCH', 'PUT')]
-        [Alias('Method')]
-        [String]$Command,
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [String]$URI,
-        [Parameter(Mandatory = $false)]
-        [Alias('Headers')]
-        $Header,
-        [Parameter(Mandatory = $false)]
-        $Body,
-        [Parameter(Mandatory = $false)]
-        [ValidateSet('Continue', 'Ignore', 'Inquire', 'SilentlyContinue', 'Stop', 'Suspend')]
-        [String]$ErrAction = 'Continue',
-        [Parameter(Mandatory = $false)]
-        [int]$TimeoutSec = 2700,
-        [Parameter(Mandatory = $false)]
-        [string]$ContentType = 'application/json'
+	param (
+		[Parameter(Mandatory = $true)]
+		[ValidateSet('GET', 'POST', 'DELETE', 'PATCH', 'PUT')]
+		[Alias('Method')]
+		[String]$Command,
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[String]$URI,
+		[Parameter(Mandatory = $false)]
+		[Alias('Headers')]
+		$Header,
+		[Parameter(Mandatory = $false)]
+		$Body,
+		[Parameter(Mandatory = $false)]
+		[ValidateSet('Continue', 'Ignore', 'Inquire', 'SilentlyContinue', 'Stop', 'Suspend')]
+		[String]$ErrAction = 'Continue',
+		[Parameter(Mandatory = $false)]
+		[int]$TimeoutSec = 2700,
+		[Parameter(Mandatory = $false)]
+		[string]$ContentType = 'application/json'
 
-    )
-    Write-LogMessage -type Verbose -MSG "Invoke-Rest:`tStart"
-    $restResponse = ''
-    try {
-        if ([string]::IsNullOrEmpty($Body)) {
-            Write-LogMessage -type Verbose -MSG "Invoke-Rest:`tInvoke-RestMethod -Uri $URI -Method $Command -Header $($Header|ConvertTo-Json -Compress) -ContentType $ContentType -TimeoutSec $TimeoutSec"
-            $restResponse = Invoke-RestMethod -Uri $URI -Method $Command -Header $Header -ContentType $ContentType -TimeoutSec $TimeoutSec -ErrorAction $ErrAction -Verbose:$false -Debug:$false
-        }
-        else {
-            Write-LogMessage -type Verbose -MSG "Invoke-Rest:`tInvoke-RestMethod -Uri $URI -Method $Command -Header $($Header|ConvertTo-Json -Compress) -ContentType $ContentType -Body $($Body|ConvertTo-Json -Compress) -TimeoutSec $TimeoutSec"
-            $restResponse = Invoke-RestMethod -Uri $URI -Method $Command -Header $Header -ContentType $ContentType -Body $Body -TimeoutSec $TimeoutSec -ErrorAction $ErrAction -Verbose:$false -Debug:$false
-        }
-        Write-LogMessage -type Verbose -MSG "Invoke-Rest:`tInvoke-RestMethod completed without error"
-    }
-    catch [System.Net.WebException] {
-        Write-LogMessage -type Verbose -MSG "Invoke-Rest:`tCaught WebException"
-        if ($ErrAction -match ('\bContinue\b|\bInquire\b|\bStop\b|\bSuspend\b')) {
-            Write-LogMessage -type Error -MSG "Error Message: $_"
-            Write-LogMessage -type Error -MSG "Exception Message: $($_.Exception.Message)"
-            Write-LogMessage -type Error -MSG "Status Code: $($_.Exception.Response.StatusCode.value__)"
-            Write-LogMessage -type Error -MSG "Status Description: $($_.Exception.Response.StatusDescription)"
-            $restResponse = $null
-            Throw
-            Else {
-                Throw $PSItem
-            }
-        }
-        Else {
-            Throw $PSItem
-        }
-    }
+	)
+	Write-LogMessage -type Verbose -MSG "Invoke-Rest:`tStart"
+	$restResponse = ''
+	try {
+		if ([string]::IsNullOrEmpty($Body)) {
+			Write-LogMessage -type Verbose -MSG "Invoke-Rest:`tInvoke-RestMethod -Uri $URI -Method $Command -Header $($Header|ConvertTo-Json -Compress) -ContentType $ContentType -TimeoutSec $TimeoutSec"
+			$restResponse = Invoke-RestMethod -Uri $URI -Method $Command -Header $Header -ContentType $ContentType -TimeoutSec $TimeoutSec -ErrorAction $ErrAction -Verbose:$false -Debug:$false
+		}
+		else {
+			Write-LogMessage -type Verbose -MSG "Invoke-Rest:`tInvoke-RestMethod -Uri $URI -Method $Command -Header $($Header|ConvertTo-Json -Compress) -ContentType $ContentType -Body $($Body|ConvertTo-Json -Compress) -TimeoutSec $TimeoutSec"
+			$restResponse = Invoke-RestMethod -Uri $URI -Method $Command -Header $Header -ContentType $ContentType -Body $Body -TimeoutSec $TimeoutSec -ErrorAction $ErrAction -Verbose:$false -Debug:$false
+		}
+		Write-LogMessage -type Verbose -MSG "Invoke-Rest:`tInvoke-RestMethod completed without error"
+	}
+	catch [System.Net.WebException] {
+		Write-LogMessage -type Verbose -MSG "Invoke-Rest:`tCaught WebException"
+		if ($ErrAction -match ('\bContinue\b|\bInquire\b|\bStop\b|\bSuspend\b')) {
+			Write-LogMessage -type Error -MSG "Error Message: $_"
+			Write-LogMessage -type Error -MSG "Exception Message: $($_.Exception.Message)"
+			Write-LogMessage -type Error -MSG "Status Code: $($_.Exception.Response.StatusCode.value__)"
+			Write-LogMessage -type Error -MSG "Status Description: $($_.Exception.Response.StatusDescription)"
+			$restResponse = $null
+			Throw
+			Else {
+				Throw $PSItem
+			}
+		}
+		Else {
+			Throw $PSItem
+		}
+	}
 
-    catch [Microsoft.PowerShell.Commands.HttpResponseException] {
-        Write-LogMessage -type Verbose -MSG "Invoke-Rest:`tCaught HttpResponseException"
-        $Details = ($PSItem.ErrorDetails.Message | ConvertFrom-Json)
-        If ('SFWS0007' -eq $Details.ErrorCode) {
-            Write-LogMessage -type Verbose -MSG "Invoke-Rest:`t$($Details.ErrorMessage)"
-            Throw $PSItem
-        }
-        elseif ('PASWS013E' -eq $Details.ErrorCode) {
-            Write-LogMessage -type Error -MSG "$($Details.ErrorMessage)" -Header -Footer
-            exit 5
-        }
-        elseif ('SFWS0002' -eq $Details.ErrorCode) {
-            Write-LogMessage -type Warning -MSG "$($Details.ErrorMessage)"
-            Throw "$($Details.ErrorMessage)"
-        }
-        If ('SFWS0012' -eq $Details.ErrorCode) {
-            Write-LogMessage -type Verbose -MSG "Invoke-Rest:`t$($Details.ErrorMessage)"
-            Throw $PSItem
-        }
-        Else {
-            Write-LogMessage -type Error -MSG "Error in running $Command on '$URI', $_.Exception"
-            Throw $(New-Object System.Exception ("Invoke-Rest: Error in running $Command on '$URI'", $_.Exception))
-        }
-    }
-    catch {
-        Write-LogMessage -type Verbose -MSG "Invoke-Rest:`tCaught Exception"
-        Write-LogMessage -type Error -MSG "Error in running $Command on '$URI', $_.Exception"
-        Throw $(New-Object System.Exception ("Error in running $Command on '$URI'", $_.Exception))
-    }
-    Write-LogMessage -type Verbose -MSG "Invoke-Rest:`tResponse: $restResponse"
-    return $restResponse
+	catch [Microsoft.PowerShell.Commands.HttpResponseException] {
+		Write-LogMessage -type Verbose -MSG "Invoke-Rest:`tCaught HttpResponseException"
+		$Details = ($PSItem.ErrorDetails.Message | ConvertFrom-Json)
+		If ('SFWS0007' -eq $Details.ErrorCode) {
+			Write-LogMessage -type Verbose -MSG "Invoke-Rest:`t$($Details.ErrorMessage)"
+			Throw $PSItem
+		}
+		elseif ('PASWS013E' -eq $Details.ErrorCode) {
+			Write-LogMessage -type Error -MSG "$($Details.ErrorMessage)" -Header -Footer
+			exit 5
+		}
+		elseif ('SFWS0002' -eq $Details.ErrorCode) {
+			Write-LogMessage -type Warning -MSG "$($Details.ErrorMessage)"
+			Throw "$($Details.ErrorMessage)"
+		}
+		If ('SFWS0012' -eq $Details.ErrorCode) {
+			Write-LogMessage -type Verbose -MSG "Invoke-Rest:`t$($Details.ErrorMessage)"
+			Throw $PSItem
+		}
+		Else {
+			Write-LogMessage -type Error -MSG "Error in running $Command on '$URI', $_.Exception"
+			Throw $(New-Object System.Exception ("Invoke-Rest: Error in running $Command on '$URI'", $_.Exception))
+		}
+	}
+	catch {
+		Write-LogMessage -type Verbose -MSG "Invoke-Rest:`tCaught Exception"
+		Write-LogMessage -type Error -MSG "Error in running $Command on '$URI', $_.Exception"
+		Throw $(New-Object System.Exception ("Error in running $Command on '$URI'", $_.Exception))
+	}
+	Write-LogMessage -type Verbose -MSG "Invoke-Rest:`tResponse: $restResponse"
+	return $restResponse
 }
 Function Add-SearchCriteria {
 	param ([string]$sURL, [string]$sSearch, [string]$sSortParam, [string]$sSafeName, [int]$iLimitPage, [int]$iOffsetPage)
@@ -685,7 +685,7 @@ Function Add-AccountLink {
 			$retResult = $true
 		}
 	}
- catch {
+	catch {
 		Throw $PSitem
 	}
 	return $retResult
